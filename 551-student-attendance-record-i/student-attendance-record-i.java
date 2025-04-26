c30a8f59b74bd8e6aa9b8dde17528ac5e89ef03e
GopikaV-123 <23bit015@kprcas.ac.in>
2025-04-26 09:54:08 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (92.95%) - LeetSync

---
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkRecord(String s) {
+       int a=0;
+       for(int i=0;i<s.length();i++) {
+        if(s.charAt(i)=='A'){
+            a++;
+        }
+        if(a>=2){
+            return false;
+        }
+       } 
+       if(s.contains("LLL")){
+        return false;
+       }
+       return true;
+    }
+}
