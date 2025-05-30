74990ffde3af2125f7c2855bf8d4c615dac11cfa
GopikaV-123 <23bit015@kprcas.ac.in>
2025-05-30 14:46:07 +0530

    Time: 1 ms (99.47%) | Memory: 59.4 MB (51.67%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+         if(nums[0]<nums[nums.length-1]){
+            for(int i=1;i<nums.length;i++){
+                if(nums[i-1]<=nums[i]) continue;
+                else return false;
+            }
+        }
+        else{
+            for(int i=1;i<nums.length;i++){
+                if(nums[i-1]>=nums[i]) continue;
+                else return false;
+            }
+        }
+        return true;
+    }
+}
