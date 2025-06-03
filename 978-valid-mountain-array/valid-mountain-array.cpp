d44968822a68f799f4736751cd5f6c7ee245a104
GopikaV-123 <23bit015@kprcas.ac.in>
2025-06-03 16:35:09 +0530

    Time: 20 ms (51.17%) | Memory: 26.1 MB (89.48%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+          int n = arr.size();
+        if(n<3){
+            return false;
+        }
+        int i = 0;
+        for(;i<n-1;i++){
+            if(arr[i]==arr[i+1]){
+                return false;
+            }
+            if(arr[i]>arr[i+1]){
+                break;
+            }
+        }
+        if(i==0 || i==n-1){
+            return false;
+        }
+        for(;i<n-1;i++){
+            if(arr[i]==arr[i+1]){
+                return false;
+            }
+            if(arr[i]<arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
