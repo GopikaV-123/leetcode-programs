87c777a6118b3e59d7fe43d321453d0f8c4e5ae1
GopikaV-123 <23bit015@kprcas.ac.in>
2025-06-03 10:53:59 +0530

    Time: 1 ms (10.06%) | Memory: 42.9 MB (44.91%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,11 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-     int largest = accounts[0][0];
-        for(int customer[] : accounts){
-            int currentSum = 0;
-            for(int bank : customer) currentSum += bank;
-            largest = Math.max(largest, currentSum);
+        int max = 0;
+        for(int i=0; i<accounts.length; i++){
+            int rowsum = 0;
+            for(int j=0; j<accounts[i].length; j++){
+                rowsum += accounts[i][j];
+                if(rowsum > max){
+                    max = rowsum;
+                }
+            }
         }
-        return largest;   
+        return max;
     }
 }
