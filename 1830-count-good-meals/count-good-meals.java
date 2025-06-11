baeaddb7d9a0521d9dfbea2bd747776b082c7dac
GopikaV-123 <23bit015@kprcas.ac.in>
2025-06-11 10:10:04 +0530

    Time: 16 ms (99.66%) | Memory: 63.4 MB (5.44%) - LeetSync

--- 1830-count-good-meals/count-good-meals.java
+++ 1830-count-good-meals/count-good-meals.java
@@ -1,30 +1,23 @@
 class Solution {
     public int countPairs(int[] deliciousness) {
-         int MOD = 1_000_000_007;
-        int maxVal = 0;
-
-        // Find max value in deliciousness
-        for (int val : deliciousness) {
-            if (val > maxVal) maxVal = val;
-        }
-
-        // Frequency array size: max possible deliciousness value + 1
-        int maxSize = maxVal + 1;
-        int[] freq = new int[maxSize];
-
-        long count = 0;
-
-        for (int val : deliciousness) {
-            // Check for all powers of two up to max possible sum (maxVal * 2)
-            for (int sum = 1; sum <= maxVal * 2; sum <<= 1) {
-                int complement = sum - val;
-                if (complement >= 0 && complement < maxSize) {
-                    count += freq[complement];
-                }
+          int MOD = 1_000_000_007;
+    int maxVal = 0;
+    for (int i = 0; i < deliciousness.length; i++) {
+        if (deliciousness[i] > maxVal) maxVal = deliciousness[i];
+    }
+    int maxSize = maxVal + 1;
+    int[] freq = new int[maxSize];
+    long count = 0;
+    for (int i = 0; i < deliciousness.length; i++) {
+        int val = deliciousness[i];
+        for (int sum = 1; sum <= maxVal * 2; sum <<= 1) {
+            int complement = sum - val;
+            if (complement >= 0 && complement < maxSize) {
+                count += freq[complement];
             }
-            freq[val]++;
         }
-
-        return (int) (count % MOD);
+        freq[val]++;
+    }
+    return (int) (count % MOD);
     }
 }
